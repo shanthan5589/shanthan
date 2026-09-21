314b9ef27e608461c9060fdbe81f1f5dade133f3
Shanthan <shanthan.yxo@gmail.com>
2026-09-21 08:40:51 +0000

    Add mesoGPT blog

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/mesoGPT",
+        destination: "/mesoGPT/index.html",
+      },
+    ];
+  },
 };
 
 export default config;
